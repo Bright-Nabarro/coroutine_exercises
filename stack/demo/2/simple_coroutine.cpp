e618e353c829d4bc5095fdeb66b501df4aca44dd
yinqian.lin <yinqian.lin@boomingtech.com>
2025-11-18 21:38:55 +0800

    fix: 修复stack/demo/2 fiber侧逻辑

--- stack/demo/2/simple_coroutine.cpp
+++ stack/demo/2/simple_coroutine.cpp
@@ -11,7 +11,13 @@ Coroutine::Coroutine()
 	  m_stack{nullptr}
 #endif
 {
-	// 对于ucontext, 不在这里初始化, swap时会接受上下文
+	// 对于ucontext, 不在这里初始化. swap时会接受上下文
+#ifdef CO_USE_FIBER
+	m_handle = ConvertThreadToFiber(nullptr);
+	if (!m_handle) {
+		m_handle = GetCurrentFiber();
+	}
+#endif 
 }
 
 Coroutine::Coroutine(std::function<void()> task, std::size_t stack_size)
@@ -24,12 +30,7 @@ Coroutine::Coroutine(std::function<void()> task, std::size_t stack_size)
 {
 	assert(co_list.size() >= 1);
 #ifdef CO_USE_FIBER
-	m_root_context = ConvertThreadToFiber(nullptr);
-	// 如果当前线程已经是fiber, 直接获取
-	if (!m_root_context) {
-		m_root_context = GetCurrentFiber();
-	}
-	m_context = CreateFiber(m_stack_size, context_entry, this);
+	m_handle = CreateFiber(m_stack_size, context_entry, this);
 #else
 	// 当前上下文作为初始化模版
 	getcontext(&m_handle);
@@ -48,17 +49,19 @@ Coroutine::Coroutine(std::function<void()> task, std::size_t stack_size)
 
 
 Coroutine::~Coroutine() {
+	if (this != &co_root)
+	{
 #ifdef CO_USE_FIBER
-		if (m_context) {
-			DeleteFiber(m_context);
-		}
+		DeleteFiber(m_handle);
 #endif
 		// co_list比co_root早析构，所以需要判断是否为co_root
-		assert(this == &co_root || co_list.size() >= 1);
-		assert(this == &co_root || co_list.back() == this);
+		assert(co_list.size() >= 1);
+		assert(co_list.back() == this);
 		co_list.pop_back();
+		assert(!co_list.empty());
 		co_cur_index = m_cur_index - 1;
 	}
+}
 
 
 void Coroutine::resume() {
@@ -68,27 +71,28 @@ void Coroutine::resume() {
 	}
 	assert(co_list[m_cur_index] == this);
 
+	co_cur_index = m_cur_index;
 #ifdef CO_USE_FIBER
-	SwitchToFiber(m_context);
+	SwitchToFiber(m_handle);
 #else
 	// 保存当前上下文到param1, 切换到协程上下文param2
-	co_cur_index = m_cur_index;
 	swapcontext(&co_list[m_cur_index - 1]->m_handle, &m_handle);
 #endif
 }
 
-// 通过current访问当前协程
+// 通过co_cur_index访问当前协程
 void Coroutine::yield() {
 	// 检查是否在一个协程上下文中
 	if (co_list.size() <= 1 || co_cur_index == 0) {
 		throw std::logic_error{"not in coroutine or coroutine finished"};
 	}
-#ifdef CO_USE_FIBER
-	SwitchToFiber(co_current->m_root_context);
-#else
-	// 保存状态到m_current->m_context, 切换到主函数上下文
+
 	std::size_t ori_index = co_cur_index;
 	co_cur_index -= 1;
+#ifdef CO_USE_FIBER
+	// 切换到当前fiber上一层的fiber
+	SwitchToFiber(co_list[ori_index-1]->m_handle);
+#else
 	// 保存当前上下文到param1, 切换到协程上下文param2
 	swapcontext(&co_list[ori_index]->m_handle,
 				&co_list[ori_index - 1]->m_handle);
@@ -102,25 +106,34 @@ void Coroutine::yield() {
  */
 void CALLBACK Coroutine::context_entry(void* param) {
 	auto* co_current = static_cast<Coroutine*>(param);
-	assert(!co_list.empty()) assert(co_list.back() == co);
+	assert(!co_list.empty());
+	assert(co_cur_index == co_list.size() - 1);
+	assert(co_list.back() == param);
 	try {
 		co_current->m_task();
 	} catch (...) {
-		std::println("excepted exception");
 		co_current->m_finished = true;
 		throw;
 	}
 	co_current->m_finished = true;
-	// 切换回主函数
-	SwitchToFiber(co_current->m_root_context);
+	// 切换回上一级
+	SwitchToFiber(co_list[co_current->m_cur_index-1]->m_handle);
+	// co_cur_index 已经在析构函数中更新
 }
 
 #else
 
 void Coroutine::context_entry() {
 	assert(co_list.size() > 1);
+	assert(co_cur_index == co_list.size() - 1);
 	auto* co_current = co_list.back();
-	co_current->m_task();
+	try {
+		co_current->m_task();
+	}
+	catch (...) {
+		co_current->m_finished = true;
+		throw;
+	}
 	co_current->m_finished = true;
 	// 函数返回时， 使用uc_link中设置的上下文，切换回主函数
 	// co_cur_index 已经在析构函数中更新
